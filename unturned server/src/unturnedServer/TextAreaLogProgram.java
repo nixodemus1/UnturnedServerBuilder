280c5d91c8e56302e5d6d04d9064f2a7a1419648
Nick Chowa <45222569+nixodemus1@users.noreply.github.com>
2021-09-27 14:00:46 -0400

    Update TextAreaLogProgram.java
    put in some javadoc implimentation

--- unturned server/src/unturnedServer/TextAreaLogProgram.java
+++ unturned server/src/unturnedServer/TextAreaLogProgram.java
@@ -6,7 +6,7 @@
 package unturnedServer;
 
 /**
- *
+ * This takes the console and puts it in the textarea on the GUI
  * @author root
  */
 import java.awt.GridBagConstraints;
@@ -31,7 +31,7 @@ import javax.swing.text.BadLocationException;
 @SuppressWarnings("serial")
 public class TextAreaLogProgram extends JFrame {
 
-    /**
+   /**
      * The text area which is used for displaying logging information.
      */
     private JTextArea textArea;
@@ -121,37 +121,6 @@ public class TextAreaLogProgram extends JFrame {
                 @SuppressWarnings("unused")
 				String s = null;
 
-                /*try {
-
-        // run the Unix "ps -ef" command
-            // using the Runtime exec method:
-            Process p = Runtime.getRuntime().exec("dir");
-
-            BufferedReader stdInput = new BufferedReader(new 
-                 InputStreamReader(p.getInputStream()));
-
-            BufferedReader stdError = new BufferedReader(new 
-                 InputStreamReader(p.getErrorStream()));
-
-            // read the output from the command
-            System.out.println("Here is the standard output of the command:\n");
-            while ((s = stdInput.readLine()) != null) {
-                System.out.println(s);
-            }
-
-            // read any errors from the attempted command
-            System.out.println("Here is the standard error of the command (if any):\n");
-            while ((s = stdError.readLine()) != null) {
-                System.out.println(s);
-            }
-
-            System.exit(0);
-        }
-        catch (IOException e) {
-            System.out.println("exception happened - here's what I know: ");
-            e.printStackTrace();
-            System.exit(-1);
-        }*/
                 try {
                 	
                 	if (! cmd.exists()) {
